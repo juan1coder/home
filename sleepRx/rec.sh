6c0f049db9e737be7ef139dcf6ed4521fac491d3
Yonn Lopez <32426635+juan1coder@users.noreply.github.com>
2017-09-30 21:58:29 -0700

    Update rec.sh
    The script saves the file to the /tmp folder so beware before doing a reboot.

--- sleepRx/rec.sh
+++ sleepRx/rec.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
-##ffmpeg -f alsa -i sysdefault:CARD=Headset -ac 2 -c:a libvorbis -b:a 128k -ar 44100 $(date +"%d-%b-%Y_%H.%M.%S%p").ogg
 cd /tmp/
 ffmpeg -f v4l2 -video_size 320x240 -i /dev/video0 $(date +"%d-%b-%Y_%H.%M.%S%p").mkv
+###Use the following for DVD quality resolution (480)
 #ffmpeg -f v4l2 -video_size 640x480 -i /dev/video0 $(date +"%d-%b-%Y_%H.%M.%S%p").mkv
-
-#ffmpeg -f alsa -i sysdefault:CARD=Headset -ac 2 -c:a libvorbis -b:a 128k -ar 44100 $(date +"%d-%b-%Y_%H.%M.%S").ogg
-
-#ffmpeg -f alsa -i pulse -c:a libvorbis -b:a 128k -ar 44100 -ac 2 "$(date)".oga
-#ffmpeg -f alsa -i pulse -ac 2 -c:a libmp3lame -b:a 128k "$(date)".
