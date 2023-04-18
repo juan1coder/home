c677650f1a04ab55edb1f848638aacfea2e86d59
Yonn Lopez <32426635+juan1coder@users.noreply.github.com>
2023-04-18 01:54:34 -0700

    Update rec.sh

--- music/rec.sh
+++ music/rec.sh
@@ -2,4 +2,4 @@
 ###Choosee syscardefault when using alsa by default on puppy linux.
 ##ffmpeg -f alsa -i sysdefault:CARD=Headset -ac 2 -c:a libvorbis -b:a 128k -ar 44100 $(date +"%d-%b-%Y_%H.%M.%S%p").ogg
 cd ~/Music/
-ffmpeg -f alsa -i pulse -c:a libvorbis -threads 2 -ac 2 $(date +"%d-%b-%Y_%H.%M.%S").ogg
+ffmpeg -f alsa -i default -c:a libvorbis -threads 2 -ac 2 $(date +"%d-%b-%Y_%H.%M.%S").ogg
